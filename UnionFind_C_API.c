d561d370ac6e6924b286ce58991fcbb4b3edb8e6
“Guru <gurusarath1.official@gmail.com>
2020-05-26 21:44:37 -0500

    Second commit

--- UnionFind_C_API.c
+++ UnionFind_C_API.c
@@ -16,19 +16,22 @@ typedef struct UnionFind_DS {
 	int size;
 	int numberOfElements;
 	UF_Node** nodes;
+	void (*printFunc)(UF_DATA_TYPE);
 
 } UF;
 
 
 typedef enum UnionFind_Error_Codes { unknownError } UF_Error_Codes;
-UF Create_UnionFind(int size);
+UF Create_UnionFind(int size, void (*printFunc)(UF_DATA_TYPE));
 int UF_makeSet(UF* UF_obj_ptr, UF_DATA_TYPE data);
 int UF_find(UF* UF_obj_ptr, int nodeIndex);
 int UF_union(UF* UF_obj_ptr, int node1_index, int node2_index);
+void UF_print(UF* UF_obj_ptr);
+void printFloatData(UF_DATA_TYPE data);
 
 int main()
 {
-	UF unionFind_1 = Create_UnionFind(10);
+	UF unionFind_1 = Create_UnionFind(10, printFloatData);
 
 	printf("%d\n", UF_makeSet(&unionFind_1, 45));
 	printf("%d\n", UF_makeSet(&unionFind_1, 8));
@@ -39,16 +42,19 @@ int main()
 
 	printf("Find %d\n", UF_find(&unionFind_1, 0));
 
+	UF_print(&unionFind_1);
+
 	return 0;
 }
 
 
-UF Create_UnionFind(int size)
+UF Create_UnionFind(int size, void (*printFunc)(UF_DATA_TYPE) )
 {
 	UF UF_object;
 	UF_object.nodes = (UF_Node**)malloc(sizeof(UF_Node*) * size);
 	UF_object.size = size;
 	UF_object.numberOfElements = 0;
+	UF_object.printFunc = printFunc;
 	return UF_object;
 }
 
@@ -89,4 +95,24 @@ int UF_union(UF* UF_obj_ptr, int node1_index, int node2_index)
 
 	UF_obj_ptr->nodes[parent1]->parentIndex = parent2;
 	return 1;
+}
+
+void printFloatData(UF_DATA_TYPE data)
+{
+	printf("%f  ", data);
+}
+
+void UF_print(UF* UF_obj_ptr)
+{
+	printf("\n");
+	for(int i=0; i < UF_obj_ptr->numberOfElements; i++)
+	{
+		printf("\nNode: %d | Parent: %d | Data: ", i, UF_obj_ptr->nodes[i]->parentIndex);
+
+		if(UF_obj_ptr->printFunc != NULL)
+		{
+			UF_obj_ptr->printFunc(UF_obj_ptr->nodes[i]->data);
+		}
+	}
+	printf("\n");
 }
